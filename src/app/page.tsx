4aa3503986449ef46fe27dcaa1786b0a1fa4c671
Vladislav Pavlov <vldslw@gmail.com>
2023-10-20 15:04:40 -0300

    feat: add Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
-"use client"
+"use client";
 
-import { useState, useEffect } from 'react';
-import Image from 'next/image'
-import styles from './page.module.css'
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Header } from "../components/Header/Header";
 
 export default function Home() {
   const [skills, setSkills] = useState([]);
 
   useEffect(() => {
     (async () => {
-      const data: any = await fetch('/api/skills');
+      const data: any = await fetch("/api/skills");
       const dataJson = await data.json();
       setSkills(dataJson.results);
     })();
@@ -17,49 +18,7 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        {skills.map((skill: any) => (
-          <div key={skill._id} className={styles.card}>
-            <h2>{skill.title}</h2>
-            <p>{skill.desc}</p>
-          </div>
-          ))}
-      </div>
+      <Header />
     </main>
-  )
+  );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import styles from "./Header.module.scss";
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Link
+        href="/"
+        onClick={() => {
+          window.location.href = "/";
+        }}
+        className={styles.header__logo}
+      >
+        vldslw.
+      </Link>
+      <nav className={styles.header__nav}>
+        <a href="#stack" className={styles.header__link}>
+          About
+        </a>
+        <a href="#projects" className={styles.header__link}>
+          Projects
+        </a>
+        <a href="#footer" className={styles.header__link}>
+          Contact
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
